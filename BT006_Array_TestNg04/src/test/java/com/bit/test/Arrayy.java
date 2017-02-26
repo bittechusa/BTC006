04478416eaa94c6ab9353d588a6e9bd7139217ec
Heshantha Ekanayaka <Ashan@Heshanthas-MBP.home>
2017-02-25 23:45:36 -0500

    command

---
+++ BT006_Array_TestNg04/src/test/java/com/bit/test/Arrayy.java
@@ -0,0 +1,40 @@
+package com.bit.test;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+
+
+
+
+public class Arrayy  {
+	
+	
+	public  String [] menunames = {	
+
+		    "HOME",
+		    "BED & BATH",
+		    "WOMEN",
+		    "MEN",
+		    "JUNIORS",
+		    "KIDS",
+		    "ACTIVE",
+		    "BEAUTY",
+		    "SHOES",
+		    "HANDBAGS",
+		    "JEWELRY",
+		    "WATCHES",
+		    "BRANDS"
+
+	};
+}
+	
+
+	
+	
+
